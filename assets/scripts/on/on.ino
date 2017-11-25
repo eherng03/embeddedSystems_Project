38f9d32dab63a3d73b1dbedafb71e6ead0ca0076
EvaHergar <eherng03@estudiantes.unileon.es>
2017-11-25 16:47:38 +0000

    Add files via upload

---
+++ assets/scripts/on/on.ino
@@ -0,0 +1,11 @@
+#include <Wire.h>
+
+const int ledPIN = 13;
+ 
+void setup() {
+  pinMode(ledPIN , OUTPUT);  //definir pin como salida
+}
+ 
+void loop(){
+  digitalWrite(ledPIN , HIGH);   // poner el Pin en HIGH
+}
