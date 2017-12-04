d00b975c3f64ccbda613d9466cff74d5c696b2bb
Alba de Pedro López <adepel00@users.noreply.github.com>
2017-12-04 10:13:35 +0100

    models, TODO methods

---
+++ models/PlaceDBController.php
@@ -0,0 +1,19 @@
+<?php
+	include_once "../dataBase/DBManager.php";
+
+
+	//Clase que controla la tabla de luces de la base de datos
+	class PlaceDBController{
+		private static $instance;
+		private function __construct(){}
+		
+		public static function getInstance(){
+			if (!self::$instance instanceof self){
+				self::$instance = new self;
+			}
+			return self::$instance;
+		}
+
+
+	}
+?>
---
+++ models/TemperatureDBController.php
@@ -0,0 +1,28 @@
+<?php
+	include_once "../dataBase/DBManager.php";
+
+
+	//Clase que controla la tabla de luces de la base de datos
+	class TemperatureDBController{
+		private static $instance;
+		private function __construct(){}
+		
+		public static function getInstance(){
+			if (!self::$instance instanceof self){
+				self::$instance = new self;
+			}
+			return self::$instance;
+		}
+
+		/**
+		Cambia el estado de las luces de la habitacion place
+		*/
+		function changeStatus($state, $place){
+			$dbManager = DBManager::getInstance();
+			$connection = $dbManager->getConnection();
+
+			$query = $connection->query("UPDATE temperature SET state = '$state' WHERE location = '$place'");
+			return $query;
+		}
+	}
+?>
