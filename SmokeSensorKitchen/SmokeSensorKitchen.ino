9022130bc85feb3b4507a9b91d6cf3ad9815e865
Alba de Pedro López <adepel00@users.noreply.github.com>
2017-12-11 09:50:28 +0100

    Smoke sensor arduino program

---
+++ SmokeSensorKitchen/SmokeSensorKitchen.ino
@@ -0,0 +1,67 @@
+#include <DHT11.h>
+#include <ESP8266WiFi.h>
+
+DHT11 dht11(D4);
+const char* ssid = "rasp";
+const char* password = "raspberry";
+const char* host = "192.168.43.247";
+
+int DOPin = 2; // select the pin for the LED
+int ledPin =13;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(DOPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+}
+
+void loop() {  
+  float status;
+  String place = "kitchen";
+
+ if (digitalRead(DOPin) ==HIGH){
+    digitalWrite(ledPin, LOW);
+    status = 0;
+  }else {
+    digitalWrite(ledPin, HIGH);
+    status = 1;
+  }
+  delay(1000);
+  
+
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("Coneccion Fallida");
+    return;
+  }
+
+  String url = "/IntelliHome/operations/changeSmoke.php?";
+  String dato1 = "status =";
+  String dato2 = "&place =";
+
+  client.print(String("GET ") + url + dato1 + status + dato2 + place + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Coneccion: Cerrada\r\n\r\n");
+
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+    Serial.println(">>> Se acabo el tiempo de espera !");
+    client.stop();
+    return;
+  }
+
+  //Imprimimos lo que nos devuelve el servidor
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  delay(3000);
+}
