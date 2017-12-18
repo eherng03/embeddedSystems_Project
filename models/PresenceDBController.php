8113bd55d26628c5ceff31e008756da7a26308c8
EvaHergar <eherng03@estudiantes.unileon.es>
2017-12-18 11:38:17 +0000

    actualizados php

--- models/PresenceDBController.php
+++ models/PresenceDBController.php
@@ -2,7 +2,7 @@
 	include_once "../dataBase/DBManager.php";
 
 
-	//Clase que controla la tabla de luces de la base de datos
+	//Clase que controla la tabla de humo de la base de datos
 	class PresenceDBController{
 		private static $instance;
 		private function __construct(){}
@@ -15,15 +15,14 @@
 		}
 
 		/**
-		* Cambia el estado de presencia habitacion place
+		Inserta un nuevo dato de humo de la habitacion place
 		*/
 		function changeStatus($state, $place){
 			$dbManager = DBManager::getInstance();
 			$connection = $dbManager->getConnection();
 
-			$query = $connection->query("UPDATE presence SET state = '$state' WHERE location = '$place'");
+			$query = $connection->query("INSERT INTO presence (status, date, place) VALUES ($state, CURRENT_TIMESTAMP, '$place')");
 			return $query;
 		}
-
 	}
 ?>
--- models/SmokeDBController.php
+++ models/SmokeDBController.php
@@ -2,7 +2,7 @@
 	include_once "../dataBase/DBManager.php";
 
 
-	//Clase que controla la tabla de luces de la base de datos
+	//Clase que controla la tabla de humo de la base de datos
 	class SmokeDBController{
 		private static $instance;
 		private function __construct(){}
@@ -15,16 +15,14 @@
 		}
 
 		/**
-		* Cambia el estado de la presencia de humo o gas de la habitacion place
+		Inserta un nuevo dato de humo de la habitacion place
 		*/
 		function changeStatus($state, $place){
 			$dbManager = DBManager::getInstance();
 			$connection = $dbManager->getConnection();
 
-			$query = $connection->query("UPDATE temperature SET state = '$state' WHERE location = '$place'");
+			$query = $connection->query("UPDATE smoke SET status = $state, date = CURRENT_TIMESTAMP WHERE place = '$place'");
 			return $query;
 		}
-
-
 	}
 ?>
