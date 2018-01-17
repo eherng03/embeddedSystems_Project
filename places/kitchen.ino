f78c166a2647e13f4007724fc616af95c225bc51
EvaHergar <eherng03@estudiantes.unileon.es>
2018-01-17 10:58:01 +0000

    KITCHEN WORKING

--- places/kitchen.ino
+++ places/kitchen.ino
@@ -1,4 +1,7 @@
-#include <DHT11.h>
+#include <DHT.h>
+
+
+//#include <DHT11.h>
 #include <ESP8266WiFi.h>
 
 //Configuración de la red wifi y el host donde está el servidor
@@ -7,13 +10,15 @@ const char* password = "raspberry";
 const char* host = "192.168.43.247";
 
 const int LED = 16;
-DHT11 dht11(D4);
-int DOPin = 5;
+//DHT11 dht11(D4);
+int DOPin = 4;
+DHT dht;
 
 void setup() {
 
   pinMode(LED, OUTPUT);
   pinMode(DOPin, INPUT);
+  dht.setup(D4);
 
   Serial.begin(9600);
   Serial.print("Conectando a: ");
@@ -47,7 +52,7 @@ void loop()
     return;
   }
 
-  String urlLight = "/IntelliHome/operations/getLightStatus.php?place=garage";
+  String urlLight = "/IntelliHome/operations/getLightStatus.php?place=kitchen";
   String urlSmoke = "/IntelliHome/operations/changeSmoke.php?";
   
   //Luz
@@ -60,7 +65,7 @@ void loop()
   unsigned long timeout1 = millis();
   while (client.available() == 0) {
     if (millis() - timeout1 > 5000) {
-      Serial.println(">>> Se acabo el tiempo de espera puerta !");
+      Serial.println(">>> Se acabo el tiempo de espera luz !");
       client.stop();
       return;
     }
@@ -74,7 +79,7 @@ void loop()
     } else if (stat1 == 48) {
       digitalWrite(LED, LOW);
     }
-    Serial.println("Este es el dato:");
+    Serial.println("Este es el dato luz:");
     Serial.println(stat1);
   }
 
@@ -88,21 +93,15 @@ void loop()
   client.flush();
 
   Serial.print("Estado del cliente: " + client.connected());
-  if ((err = dht11.read(hum, temp)) == 0)
-  {
+    temp = dht.getTemperature();
+    hum = dht.getHumidity();
     Serial.print("Temperatura: ");
     Serial.print(temp);
     Serial.print(" Humedad: ");
     Serial.print(hum);
     Serial.print(place);
     Serial.println();
-  }
-  else
-  {
-    Serial.print("Error Num :");
-    Serial.print(err);
-    Serial.println();
-  }
+  
 
 //Humo
   if (digitalRead(DOPin) ==HIGH){
@@ -132,7 +131,7 @@ void loop()
   unsigned long timeout2 = millis();
   while (client.available() == 0) {
     if (millis() - timeout2 > 5000) {
-      Serial.println(">>> Se acabo el tiempo de espera puerta !");
+      Serial.println(">>> Se acabo el tiempo de espera humo !");
       client.stop();
       return;
     }
@@ -146,22 +145,21 @@ void loop()
 
   String urlTemp = "/IntelliHome/operations/changeTemperature.php?";
   String urlHum = "/IntelliHome/operations/changeHumidity.php?";
-  String dato1 = "temp=";
-  String dato2 = "hum=";
-  String dato3 = "&place=";
+  String datoT = "temp=";
+  String datoH = "hum=";
   Serial.print("Obteniendo URL: ");
   Serial.println(urlTemp);
   //Creamos la peticion al servidor
-  client.print(String("GET ") + urlTemp + dato1 + temp + dato3 + place + " HTTP/1.1\r\n" +
+  client.print(String("GET ") + urlTemp + datoT + temp + dato3 + place + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Coneccion: Cerrada\r\n\r\n");
-  client.print(String("GET ") + urlHum + dato2 + hum + dato3 + place + " HTTP/1.1\r\n" +
+  client.print(String("GET ") + urlHum + datoH + hum + dato3 + place + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Coneccion: Cerrada\r\n\r\n");
   unsigned long timeout3 = millis();
   while (client.available() == 0) {
     if (millis() - timeout3 > 5000) {
-      Serial.println(">>> Se acabo el tiempo de espera !");
+      Serial.println(">>> Se acabo el tiempo de espera humTemp !");
       client.stop();
       return;
     }
