1d4051ad25077bd7a01df629131458e560a0a40b
alroastorga <alroastorga@gmail.com>
2018-01-21 20:40:30 +0100

    Add files via upload

--- places/room.ino
+++ places/room.ino
@@ -7,13 +7,13 @@ const char* password = "raspberry";
 const char* host = "192.168.43.247";
 
 const int LED = 16;
-int DOPin = 2;
+int presencePin = 2;
 
 void setup() {
 
-  pinMode(DOPin, INPUT);
+  pinMode(presencePin, INPUT);
   pinMode(LED, OUTPUT);
-  
+
   Serial.begin(9600);
   Serial.print("Conectando a: ");
   Serial.println(ssid);
@@ -34,8 +34,10 @@ void loop()
   int err;
   int status;
   String place = "room";
-  int state;
- 
+  int statePresence;
+  String datoPresence = "state=";
+  String datoPlace = "&place=";
+
   Serial.print("conectando a: ");
   Serial.println(host);
   WiFiClient client;
@@ -44,16 +46,16 @@ void loop()
     Serial.println("Coneccion Fallida");
     return;
   }
-  
+
   String urlLight = "/IntelliHome/operations/getLightStatus.php?place=room";
-  
-//Luz
+
+  //Luz
 
   Serial.print("Obteniendo URL: ");
   Serial.println(urlLight);
   //Creamos la peticion al servidor
   client.println(String("GET ") + urlLight);
-    
+
   unsigned long timeout1 = millis();
   while (client.available() == 0) {
     if (millis() - timeout1 > 5000) {
@@ -64,61 +66,60 @@ void loop()
   }
 
   //Imprimimos lo que nos devuelve el servidor
- while (client.available()) {
+  while (client.available()) {
     int stat1 = client.read();  //En stat guardamos lo que nos devuelve la llamada a la bbdd
-    if(stat1 == 49){
+    if (stat1 == 49) {
       digitalWrite(LED, HIGH);
-    }else if(stat1 == 48){
+    } else if (stat1 == 48) {
       digitalWrite(LED, LOW);
     }
     Serial.println("Este es el dato:");
     Serial.println(stat1);
- }
+  }
 
-//Presence
 
-if (!client.connect(host, httpPort)) {
+
+  if (!client.connect(host, httpPort)) {
     Serial.println("Coneccion Fallida");
     return;
   }
-  
-  client.flush();
 
+  client.flush();
   Serial.print("Estado del cliente: " + client.connected());
-  if (digitalRead(DOPin) ==HIGH){
-     state = 1;
-     Serial.println("Motion detected!"); 
-     delay(1000);
-  }else {
-     state = 0;
-     delay(1000);
+
+  //Presence
+  if (digitalRead(presencePin) == HIGH) {
+    statePresence = 1;
+    Serial.println("Motion detected!");
+  } else {
+    statePresence = 0;
   }
 
-  
-  String urlPresence = "/IntelliHome/operations/changePresence.php?";
-  String dato1 = "state=";
-  String dato3 = "&place=";
-  Serial.print("Obteniendo URL: ");
-  Serial.println(urlPresence);
-  //Creamos la peticion al servidor
-  client.print(String("GET ") + urlPresence + dato1 + state + dato3 + place + " HTTP/1.1\r\n" +
-  "Host: " + host + "\r\n" +
-  "Coneccion: Cerrada\r\n\r\n");
-  unsigned long timeout2 = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout2 > 5000) {
-      Serial.println(">>> Se acabo el tiempo de espera !");
-      client.stop();
+  if (statePresence == 1) {
+    if (!client.connect(host, httpPort)) {
+      Serial.println("Conexion Fallida");
       return;
     }
-  }
-  //Imprimimos lo que nos devuelve el servidor
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
 
-  
+    client.flush();
+    Serial.print("Estado del cliente: " + client.connected());
+
+    String urlPresence = "/IntelliHome/operations/changePresence.php?";
+    Serial.print("Obteniendo URL: ");
+    Serial.println(urlPresence);
+    //Creamos la peticion al servidor
+    client.print(String("GET ") + urlPresence + datoPresence + statePresence + datoPlace + place + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Coneccion: Cerrada\r\n\r\n");
+    unsigned long timeout2 = millis();
+    while (client.available() == 0) {
+      if (millis() - timeout2 > 5000) {
+        Serial.println(">>> Se acabo el tiempo de espera !");
+        client.stop();
+        return;
+      }
+    }
+  }
 
   Serial.println();
   Serial.println("Cerrando Conexión");
